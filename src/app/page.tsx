4c481b5bd646412d5ca38434182e2facf8127123
Nick Gabe <nicolasgabrielctt@gmail.com>
2023-05-03 02:41:46 -0300

    responsivity bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,13 @@ import { Posts } from './components/Posts/Posts'
 import { useState } from 'react'
 import { Title } from './components/Title/Title';
 import { Background } from './components/Background/Background';
-import { Loading } from './components/Loading/Loading';
 
 export default function Home() {
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(true);
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
+    <main>
       <Title/>
       <Search setSearch={setSearch} setLoading={setLoading}/>
       <Posts
