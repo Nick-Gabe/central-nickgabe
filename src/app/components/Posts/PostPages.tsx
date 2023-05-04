39f484bd9357617a11a122155f0586e675db929b
Nick Gabe <nicolasgabrielctt@gmail.com>
2023-05-03 23:54:36 -0300

    ordena posts por data, arruma foco no card

--- src/app/components/Posts/PostPages.tsx
+++ src/app/components/Posts/PostPages.tsx
@@ -1,11 +1,16 @@
 import styles from "./posts.module.css";
 import { PostCard } from "../PostCard/PostCard";
 import { PostPagesProps } from "./postsTypes";
+import { Post } from "@components/PostCard/postCardTypes";
+
+const sortByDate = (a: Post, b: Post) => {
+  return new Date(a.date) < new Date(b.date) ? 1 : -1
+}
 
 export const PostPages = (props: PostPagesProps) => {
   return (
     <div className={styles.container}>
-      {props.posts.map(post => (
+      {props.posts.sort(sortByDate).map(post => (
         <PostCard key={post.id} post={post} search={props.search} />
       ))}
     </div>
