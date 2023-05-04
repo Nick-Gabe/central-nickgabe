49036cb1c2ce178a7c46b7daf99b266b997db95e
datsfilipe <datsfilipe.foss@proton.me>
2023-05-04 18:04:42 -0300

    feat(posts page): add pagination with antd (#5)

--- src/app/components/Posts/PostPages.tsx
+++ src/app/components/Posts/PostPages.tsx
@@ -2,17 +2,35 @@ import styles from "./posts.module.css";
 import { PostCard } from "../PostCard/PostCard";
 import { PostPagesProps } from "./postsTypes";
 import { Post } from "@components/PostCard/postCardTypes";
+import { Pagination } from "antd";
+import { useState } from "react";
+
+const postsPerPage = 12;
 
 const sortByDate = (a: Post, b: Post) => {
   return new Date(a.date) < new Date(b.date) ? 1 : -1
 }
 
 export const PostPages = (props: PostPagesProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
-    <div className={styles.container}>
-      {props.posts.sort(sortByDate).map(post => (
-        <PostCard key={post.id} post={post} search={props.search} />
-      ))}
-    </div>
+    <>
+      <div className={styles.container}>
+        {props.posts.sort(sortByDate).slice((currentPage - 1) * postsPerPage, currentPage * postsPerPage).map((post) => (
+          <PostCard key={post.id} post={post} search={props.search} />
+        ))}
+      </div>
+      {(
+        <Pagination
+          current={currentPage}
+          pageSize={postsPerPage}
+          total={props.posts.length}
+          onChange={setCurrentPage}
+          hideOnSinglePage
+          className="mt-4 mb-2"
+        />
+      )}
+    </>
   )
 }
