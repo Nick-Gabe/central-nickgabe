13e1012f44647390519f174383cd2af9079cbf47
Nícolas Gabriel <nicolasgabrielctt@gmail.com>
2023-05-03 23:33:44 -0300

    Cria feature de compartilhar pesquisa e acessibilidade (#8)
    * cria feature de compartilhar pesquisa e acessibilidade
    
    * arruma input fazendo overflow no botão

--- next.config.js
+++ next.config.js
@@ -5,7 +5,16 @@ const nextConfig = {
   },
   images: {
     domains: ["pbs.twimg.com"]
-  }
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
--- src/app/components/Search/SearchTypes.d.ts
+++ src/app/components/Search/SearchTypes.d.ts
@@ -1,6 +1,12 @@
 import { Dispatch, SetStateAction } from "react";
 
 export type SearchProps = {
+  search: string;
   setSearch: Dispatch<SetStateAction<string>>;
   setLoading: Dispatch<SetStateAction<boolean>>;
+}
+
+export type SearchButtonsProps = {
+  onClearSearch: () => void;
+  value: string;
 }
