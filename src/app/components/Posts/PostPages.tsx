2413f0ebf641b72a0602925d78c5b5e85c67066f
datsfilipe <datsfilipe.foss@proton.me>
2023-05-06 02:51:04 -0300

    Refactor: add Providers client component for global usage

--- src/app/components/Posts/PostPages.tsx
+++ src/app/components/Posts/PostPages.tsx
@@ -1,7 +1,7 @@
 import { PostCard } from "../PostCard/PostCard";
 import { PostPagesProps } from "./postsTypes";
 import { Post } from "@components/PostCard/postCardTypes";
-import { Pagination, ConfigProvider } from "antd";
+import { Pagination } from "antd";
 import { useState } from "react";
 
 const postsPerPage = 12;
@@ -14,17 +14,8 @@ export const PostPages = (props: PostPagesProps) => {
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Pagination: {
-            colorPrimary: "#1c1917",
-            colorPrimaryHover: "#ec4899",
-          }
-        }
-      }}
-    >
-      <div className='grid gap-10 max-w-[90vw] mb-5 sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1600px] lg:px-10'>
+    <>
+      <div className='grid gap-10 mb-5 sm:grid-cols-2 lg:grid-cols-3 lg:px-10 max-w-[90vw] lg:max-w-[1600px]'>
         {props.posts.sort(sortByDate).slice((currentPage - 1) * postsPerPage, currentPage * postsPerPage).map((post) => (
           <PostCard key={post.id} post={post} search={props.search} />
         ))}
@@ -37,6 +28,6 @@ export const PostPages = (props: PostPagesProps) => {
         hideOnSinglePage
         className="mt-4 mb-2"
       />
-    </ConfigProvider>
+    </>
   )
 }
