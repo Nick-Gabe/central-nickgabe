23db79e90c0ec81c0c0876f295ee3bb925684bdf
datsfilipe <datsfilipe.foss@proton.me>
2023-05-06 23:08:06 -0300

    refactor: store posts in state and set page posts in useEffect

--- src/app/components/Posts/PostPages.tsx
+++ src/app/components/Posts/PostPages.tsx
@@ -2,16 +2,21 @@ import { PostCard } from "../PostCard/PostCard";
 import { PostPagesProps } from "./postsTypes";
 import { Post } from "@components/PostCard/postCardTypes";
 import { Pagination } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export const PostPages = (props: PostPagesProps) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    setPosts(props.posts.slice((currentPage - 1) * postsPerPage, currentPage * postsPerPage))
+  }, [currentPage, props.posts]);
 
   return (
     <>
       <div className='grid gap-10 mb-5 sm:grid-cols-2 lg:grid-cols-3 lg:px-10 max-w-[90vw] lg:max-w-[1600px]'>
-        {props.posts.sort(sortByDate).slice((currentPage - 1) * postsPerPage, currentPage * postsPerPage).map((post) => (
+        {posts.map((post) => (
           <PostCard key={post.id} post={post} search={props.search} />
         ))}
       </div>
