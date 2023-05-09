784e13c3b0c056b3021741d547de09d882229547
vboechat <victor2005contato@gmail.com>
2023-05-08 23:38:51 -0300

    feat: add Cypress configuration files

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    chromeWebSecurity: false,
+    specPattern: "./src/**/*.cy-e2e.{ts,tsx}",
+  },
+});
