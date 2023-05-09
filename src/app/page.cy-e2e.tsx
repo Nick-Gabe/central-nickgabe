28bbe632acf6473ae758462e181e4cde8a07c11b
vboechat <victor2005contato@gmail.com>
2023-05-09 00:57:25 -0300

    fix(lint): fix lint errors

--- src/app/page.cy-e2e.tsx
+++ src/app/page.cy-e2e.tsx
@@ -1,31 +1,31 @@
-describe("App", () => {
-  beforeEach(() => cy.visit("/"));
+describe('App', () => {
+  beforeEach(() => cy.visit('/'));
 
-  it("should render successfully", () => {
-    cy.get("span").contains("CENTRAL");
-    cy.get("span").contains("nick gabe");
-    cy.get("input").should("have.attr", "placeholder", "Pesquise um tema");
+  it('should render successfully', () => {
+    cy.get('span').contains('CENTRAL');
+    cy.get('span').contains('nick gabe');
+    cy.get('input').should('have.attr', 'placeholder', 'Pesquise um tema');
   });
 
-  describe("Search", () => {
-    it("should be able to search for a post", () => {
-      cy.get("input").type("Tu usa git?");
+  describe('Search', () => {
+    it('should be able to search for a post', () => {
+      cy.get('input').type('Tu usa git?');
 
-      cy.get("span").contains("Tu usa git?");
-      cy.get("a").should("have.attr", "href");
+      cy.get('span').contains('Tu usa git?');
+      cy.get('a').should('have.attr', 'href');
     });
 
-    it("should copy link successfully", async () => {
-      cy.get("input").type("Tu usa git?");
+    it('should copy link successfully', async () => {
+      cy.get('input').type('Tu usa git?');
 
       cy.get('button[data-test-id="copy-link"]').click();
-      cy.get("span").contains("Link copiado para a área de transferência!");
+      cy.get('span').contains('Link copiado para a área de transferência!');
     });
   });
 
-  describe("Posts", () => {
-    it("should have a redirect link", () => {
-      cy.get("a").should("have.attr", "href");
+  describe('Posts', () => {
+    it('should have a redirect link', () => {
+      cy.get('a').should('have.attr', 'href');
     });
   });
 });
