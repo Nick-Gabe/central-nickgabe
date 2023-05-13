5012e7ea5312dc49d756d31e7553795d6cfb7544
Nícolas Gabriel <nicolasgabrielctt@gmail.com>
2023-05-13 14:32:53 -0300

    Fix - Agora faz trim da pesquisa e arruma bug de regex (#31)
    * agora faz trim da pesquisa e arruma bug de regex
    
    * atualiza os testes de pesquisa
    
    * deixa puppeteer opcional, roda cypress em build

--- src/app/components/TextWithHighlight/TextWithHighlight.tsx
+++ src/app/components/TextWithHighlight/TextWithHighlight.tsx
@@ -1,13 +1,21 @@
 import { TextWithHighlightProps } from './TextTypes';
 
 export const TextWithHighlight = (props: TextWithHighlightProps) => {
+  if (!props.highlight) return <span>{props.text}</span>;
+
   return (
     <span
       dangerouslySetInnerHTML={{
-        __html: props.text.replaceAll(
-          new RegExp(props.highlight, 'ig'),
-          (match) => `<span class="text-primary"/>${match}</span>`
-        ),
+        __html: props.text
+          .toLowerCase()
+          .replaceAll(
+            props.highlight.toLowerCase(),
+            (match) => `<span class="text-primary"/>${match}</span>`
+          )
+          .replaceAll(
+            new RegExp(props.highlight, 'ig'),
+            (match) => `<span class="text-primary"/>${match}</span>`
+          ),
       }}
     />
   );
