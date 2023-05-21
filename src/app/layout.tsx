584b777c8c0f3ea4f48f8e1844718de2b18efc2a
Victor Ribeiro Boechat <104161238+vboechat@users.noreply.github.com>
2023-05-21 15:54:30 -0300

    feat(seo): add metadatas (#48)
    * feat(seo): add open graph metadatas
    
    * feat: add static sitemap xml
    
    * feat: add static robots text file
    
    * feat: add metadataBase and canonical
    
    * refactor: convert title, url and description in constants above metadata
    
    * feat: add twitter metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,41 @@
 import './globals.css';
 import 'antd/dist/reset.css';
 import { Analytics } from '@vercel/analytics/react';
+import { Metadata } from 'next';
 import { PropsWithChildren } from 'react';
 import { Providers } from '@components/Providers/Providers';
 
-export const metadata = {
-  description: 'Central para reunir meus conteúdos',
-  title: 'Central Nick Gabe',
+const title = 'Central Nick Gabe';
+const description = 'Central para reunir meus conteúdos';
+const url = 'https://nickgabe.vercel.app';
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: url,
+  },
+  description,
+  metadataBase: new URL(url),
+  openGraph: {
+    description,
+    images: [
+      /* TODO: Add images */
+    ],
+    locale: 'pt-BR',
+    siteName: title,
+    title,
+    type: 'website',
+    url: url,
+  },
+  title,
+  twitter: {
+    creator: '@ImNickGabe',
+    creatorId: '930575742060154885',
+    description,
+    images: [
+      /* TODO: Add images */
+    ],
+    title,
+  },
 };
 
 export default function RootLayout(props: PropsWithChildren) {
