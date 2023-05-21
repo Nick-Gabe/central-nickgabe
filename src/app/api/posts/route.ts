218098b2a1dfb8e503f1d7c191fd8ce53ccb76ed
datsfilipe <datsfilipe.foss@proton.me>
2023-05-21 11:20:43 -0300

    fix: show search in description
    When searching for a post, the description was not changing.
    The static description was being used and it sometimes doesn't had the
    searched term that is in the `full` property. So, now in the search
    route, the description is set to include the searched term, and the ellipsis.

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -7,16 +7,44 @@ export async function GET(req: Request) {
   try {
     const regex = new RegExp(search, 'ig');
 
-    return new Response(
-      JSON.stringify(
-        posts.filter((post) => {
-          return (
-            regex.test(post.full) ||
-            post.full.toLowerCase().includes(search.toLowerCase())
-          );
-        })
-      )
-    );
+    const filteredPosts = posts
+      .filter((post) => {
+        return (
+          regex.test(post.full) ||
+          post.full.toLowerCase().includes(search.toLowerCase())
+        );
+      })
+      // update description
+      .map((post) => {
+        if (!search) return post;
+
+        const allWords = post.full.split(' ').map((word) => {
+          if (word.includes('\n')) {
+            const split = word.split('\n');
+            return split[split.length - 1] + '\n';
+          }
+          return word;
+        });
+
+        const index = allWords.findIndex((word) => regex.test(word));
+        const start = index - 2;
+        const end = allWords.length - 1;
+        let description = allWords.slice(start, end).join(' ');
+
+        description =
+          description.length > 100 ? description.slice(0, 100) : description;
+
+        const firstWord = description.split(' ')[0];
+        if (firstWord !== post.description.split(' ')[0])
+          description = '...' + description;
+
+        return {
+          ...post,
+          description,
+        };
+      });
+
+    return new Response(JSON.stringify(filteredPosts));
   } catch (error) {
     return new Response('[]');
   }
