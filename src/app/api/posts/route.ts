1ba82e7b0225d3850559a561f88a2376918b41a9
datsfilipe <datsfilipe.foss@proton.me>
2023-05-22 09:56:15 -0300

    fix: ellipsis in the beginning not working as expected
    Added a verification for the first word, so if it's an emoji, a new line char or a whitespace, it will not add the ellipsis. Also if index is 0, it will not add the ellipsis.

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,7 @@
 import posts from '@public/posts.json';
 
+const emojiRegex = new RegExp('^[\\n\\s\\p{Emoji}]', 'u');
+
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const search = searchParams.get('search') || '';
@@ -40,7 +42,15 @@ export async function GET(req: Request) {
           description.length > 100 ? description.slice(0, 100) : description;
 
         const firstWord = description.split(' ')[0];
-        if (firstWord !== post.description.split(' ')[0])
+        if (
+          index > 0 &&
+          // verify if first word is a new line, an emoji or whitespace
+          !(
+            firstWord.match(emojiRegex) ||
+            firstWord === '\n' ||
+            firstWord === ' '
+          )
+        )
           description = '...' + description;
 
         return {
