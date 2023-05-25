eec4ca6055e3aab7a6ac43751049e43e2874539e
Filipe Lima <datsfilipe.foss@proton.me>
2023-05-25 01:08:59 -0300

    fix: show search in description (#56)
    * fix: show search in description
    
    When searching for a post, the description was not changing.
    The static description was being used and it sometimes doesn't had the
    searched term that is in the `full` property. So, now in the search
    route, the description is set to include the searched term, and the ellipsis.
    
    * refactor: do not include title in words list
    
    * refactor: validate if search term in present outside of title
    
    * fix: add includeSearch function to check for the search term
    
    Accepting reviewer suggestion.
    
    Co-authored-by: Nícolas Gabriel <nickygamer023@gmail.com>
    
    * fix: prettier problems
    
    * fix: use includeSearch to find search term index
    
    * refactor: more words in beginning if text is too short
    
    Also avoid a negative start.
    
    * fix: ellipsis in the beginning not working as expected
    
    Added a verification for the first word, so if it's an emoji, a new line char or a whitespace, it will not add the ellipsis. Also if index is 0, it will not add the ellipsis.
    
    ---------
    
    Co-authored-by: Nícolas Gabriel <nickygamer023@gmail.com>

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,7 @@
 import posts from '@public/posts.json';
 
+const emojiRegex = new RegExp('^[\\n\\s\\p{Emoji}]', 'u');
+
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const search = searchParams.get('search') || '';
@@ -7,16 +9,57 @@ export async function GET(req: Request) {
   try {
     const regex = new RegExp(search, 'ig');
 
-    return new Response(
-      JSON.stringify(
-        posts.filter((post) => {
-          return (
-            regex.test(post.full) ||
-            post.full.toLowerCase().includes(search.toLowerCase())
-          );
-        })
-      )
-    );
+    const includesSearch = (text: string): boolean => {
+      return (
+        regex.test(text) || text.toLowerCase().includes(search.toLowerCase())
+      );
+    };
+
+    const filteredPosts = posts
+      .filter((post) => includesSearch(post.full))
+      // update description
+      .map((post) => {
+        if (!search) return post;
+        const fullTextWithoutTitle = post.full.replace(post.title, '');
+        if (!includesSearch(fullTextWithoutTitle)) return post;
+
+        const allWords = fullTextWithoutTitle.split(' ').map((word) => {
+          if (word.includes('\n')) {
+            const split = word.split('\n');
+            return split[split.length - 1] + '\n';
+          }
+          return word;
+        });
+
+        const index = allWords.findIndex((word) => includesSearch(word));
+        if (index === -1) return post;
+        const after = allWords.slice(index).join(' ').length;
+        const start = after > 100 ? index - 2 : index - 6; // more words before if after is too big
+        const end = allWords.length - 1;
+        let description = allWords.slice(start < 0 ? 0 : start, end).join(' ');
+
+        description =
+          description.length > 100 ? description.slice(0, 100) : description;
+
+        const firstWord = description.split(' ')[0];
+        if (
+          index > 0 &&
+          // verify if first word is a new line, an emoji or whitespace
+          !(
+            firstWord.match(emojiRegex) ||
+            firstWord === '\n' ||
+            firstWord === ' '
+          )
+        )
+          description = '...' + description;
+
+        return {
+          ...post,
+          description,
+        };
+      });
+
+    return new Response(JSON.stringify(filteredPosts));
   } catch (error) {
     return new Response('[]');
   }
