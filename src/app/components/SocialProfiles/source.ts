47b50a8c60fa11f4e4982b4e2555d4c078c48351
rwietter <rwietter@zohomail.com>
2023-10-06 14:57:20 -0300

    feat: social profile type definitions and data source

---
+++ src/app/components/SocialProfiles/source.ts
@@ -0,0 +1,17 @@
+export const links: SocialProfile[] = [
+  {
+    icon: 'social/github.svg',
+    name: 'GitHub',
+    url: 'https://github.com/nick-gabe',
+  },
+  {
+    icon: 'social/linkedin.svg',
+    name: 'LinkedIn',
+    url: 'https://br.linkedin.com/in/nickgabe',
+  },
+  {
+    icon: 'social/twitter.svg',
+    name: 'Twitter',
+    url: 'https://twitter.com/ImNickGabe',
+  },
+];
